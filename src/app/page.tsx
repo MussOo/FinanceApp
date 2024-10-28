86508aa1614f282a336c04503d21391b9765fd09
Salmi Mustapha <mustapha.salmi08000@gmail.com>
2024-10-28 04:15:20 +0100

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
-'use client';
+"use client";
+import Footer from "@/components/Footer";
 import Main from "@/components/Main";
 import Navbar from "@/components/Navbar";
 import store from "@/context/store";
 import { Provider } from "react-redux";
 
 export default function Home() {
-
   return (
     <>
-    <Provider store={store}>
-      <Navbar/>
-      <Main />
-    </Provider>
+      <Provider store={store}>
+        <Navbar />
+        <Main />
+        <Footer />
+      </Provider>
     </>
   );
 }
